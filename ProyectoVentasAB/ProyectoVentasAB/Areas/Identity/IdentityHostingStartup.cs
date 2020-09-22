42c5713fd4b8c9902d80c6b611f9c5c28749ee9b
nancycmartinez <a_dramartinez@hotmail.com>
2020-09-21 23:39:51 -0600

    Cambio en el Menu y Accesos a cuenta

---
+++ ProyectoVentasAB/ProyectoVentasAB/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProyectoVentasAB.Data;
+
+[assembly: HostingStartup(typeof(ProyectoVentasAB.Areas.Identity.IdentityHostingStartup))]
+namespace ProyectoVentasAB.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
