ad8375cfd8600b3951d3de3b49d9f3e77f4ac695
nancycmartinez <a_dramartinez@hotmail.com>
2020-09-21 22:48:19 -0600

    Agrego CRUD Categorias y controller

---
+++ ProyectoVentasAB/ProyectoVentasAB/Controllers/CategoriasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProyectoVentasAB.Data;
+using ProyectoVentasAB.Models;
+
+namespace ProyectoVentasAB.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Categorias
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categorias.ToListAsync());
+        }
+
+
+        // GET: Categorias/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categorias/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdCategoria,NombreCategoria,Descripcion")] tblCategorias tblCategorias)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tblCategorias);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tblCategorias);
+        }
+
+        // GET: Categorias/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tblCategorias = await _context.Categorias.FindAsync(id);
+            if (tblCategorias == null)
+            {
+                return NotFound();
+            }
+            return View(tblCategorias);
+        }
+
+        // POST: Categorias/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdCategoria,NombreCategoria,Descripcion")] tblCategorias tblCategorias)
+        {
+            if (id != tblCategorias.IdCategoria)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tblCategorias);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!tblCategoriasExists(tblCategorias.IdCategoria))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tblCategorias);
+        }
+
+        // GET: Categorias/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tblCategorias = await _context.Categorias
+                .FirstOrDefaultAsync(m => m.IdCategoria == id);
+            if (tblCategorias == null)
+            {
+                return NotFound();
+            }
+
+            return View(tblCategorias);
+        }
+
+        // POST: Categorias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tblCategorias = await _context.Categorias.FindAsync(id);
+            _context.Categorias.Remove(tblCategorias);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool tblCategoriasExists(int id)
+        {
+            return _context.Categorias.Any(e => e.IdCategoria == id);
+        }
+    }
+}
